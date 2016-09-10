fb7832f3b75c8b3789436f8490b78756fcbe6056
Jake Seaton <jakeseaton13@gmail.com>
2016-09-10 15:03:29 -0400

    modify gitignore

--- fop/settings.py
+++ fop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_common',
+    'django_common',
 ]
 
 MIDDLEWARE_CLASSES = [
