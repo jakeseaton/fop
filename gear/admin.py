c8e947cc719c5026c11384ab8d6e721a75dd3c23
Jake Seaton <jakeseaton13@gmail.com>
2016-09-11 13:59:10 -0400

    change gear

--- gear/admin.py
+++ gear/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from models import *
 
-map(admin.site.register, [Gear, Request])
+map(admin.site.register, [Item, Request])
 # Register your models here.
--- gear/models.py
+++ gear/models.py
@@ -5,12 +5,12 @@ from mixins import TimeStamp
 
 # Create your models here.
 
-class Gear(TimeStamp):
+class Item(TimeStamp):
     name = models.CharField(max_length=255)
     count = models.IntegerField(default=0)
 
 
 class Request(TimeStamp):
     status = models.BooleanField(default=False)
-    person = models.ForeignKey("person.Student")
-    gear = models.ForeignKey(Gear, related_name="requests")
+    person = models.ForeignKey("person.Student", related_name="gear_requests")
+    gear = models.ForeignKey(Item, related_name="requests")
