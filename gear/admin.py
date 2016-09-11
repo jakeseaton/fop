064a5de1bcde6da50910cf1e226f27cb83c99cac
Jake Seaton <jakeseaton13@gmail.com>
2016-09-11 13:57:45 -0400

    gear stuff

--- gear/admin.py
+++ gear/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from models import *
 
+map(admin.site.register, [Gear, Request])
 # Register your models here.
--- gear/models.py
+++ gear/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from mixins import TimeStamp
 
 # Create your models here.
+
+class Gear(TimeStamp):
+    name = models.CharField(max_length=255)
+    count = models.IntegerField(default=0)
+
+
+class Request(TimeStamp):
+    status = models.BooleanField(default=False)
+    person = models.ForeignKey("person.Student")
+    gear = models.ForeignKey(Gear, related_name="requests")
