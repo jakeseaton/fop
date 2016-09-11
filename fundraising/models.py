a81e98b0573fbd47cb59a3e668534760d7518cad
Jake Seaton <jakeseaton13@gmail.com>
2016-09-11 13:03:14 -0400

    donation models

--- fundraising/models.py
+++ fundraising/models.py
@@ -1,11 +1,24 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from mixins import TimeStamp
+from common import enum
+
+class SolicitationMethod(enum.Enum):
+    email = enum.Item(1, "Email")
+
+
+class SolicitationCategory(enum.Enum):
+    '''
+    I have no clue what the categories of solicitation are.
+    '''
+    money = enum.Item(1, "Money")
 
 # Create your models here.
+class Solicitation(TimeStamp):
+    person = models.ForeignKey("person.Person", related_name="solicitations")
+    method = models.IntegerField(choices=SolicitationMethod, default=SolicitationMethod.email)
+    category = models.IntegerField(choices=SolicitationCategory, default=SolicitationCategory.money)
 
-# class Solicitation(models.Model):
-#     person = models.ForeignKey("person.Person")
 
-# class Donation(models.Model):
-#     person = models.ForeignKey("person.Person")
+class Donation(models.Model):
+    person = models.ForeignKey("person.Person", related_name="donations")
+    amount = models.IntegerField()
