bdb2a27a7ef6b91f3c58903aa1c08466300346da
Jake Seaton <jakeseaton13@gmail.com>
2016-09-10 23:21:27 -0400

    wtf

--- assets/js/components/Planner.jsx
+++ assets/js/components/Planner.jsx
@@ -1,26 +1,28 @@
 import React, {Component} from "react"
-import {Row, Col} from "react-bootstrap"
+import {Row, Col, ListGroup, ListGroupItem} from "react-bootstrap"
 
 const tripData = [
-	{id: 1, number: 75},
-	{id: 2, number: 100},
-	{id: 3, number: 1000}
+	{id: 1, number: 75, foppers: ["Derp", "Herp", "Yo"]},
+	{id: 2, number: 100, foppers: ["Derp", "Herp", "Yo"]},
+	{id: 3, number: 1000, foppers: ["Derp", "Herp", "Yo"]}
 ]
 
 class Trip extends Component{
 	render(){
-		return <Col xs={4} className="text-center">{this.props.trip.number}</Col>
+		return(
+			<Col xs={4}>
+				<ListGroup>
+					{this.props.trip.number}
+					{this.props.trip.foppers.map((fopper, i) => <ListGroupItem key={i}>{fopper}</ListGroupItem>)}
+				</ListGroup>
+			</Col>)
 	}
 }
 export default class Planner extends Component{
 	render(){
 		return(
 			<Row id="planner">
-				{
-					tripData.map(
-						trip => <Trip trip={trip} key={trip.id}/>
-					)
-				}
+				{tripData.map(trip => <Trip trip={trip} key={trip.id}/>)}
 			</Row>
 		)
 	}
